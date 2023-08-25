1f5951a5c7963d240b96143fcec85d4a5f058a9d
Alex <aaaa.aaaa.aaa.aaaa@mail.ru>
2023-08-25 13:16:52 +0500

    fix: change host of application

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { User, User_info } from './users/users.model';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env'
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'containers-us-west-77.railway.app',
